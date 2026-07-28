e3e2cbc4abb3525137139616634b54315d02752d
Vivekanand-s-naik <vivekanadsnaik.iet@srinivasuniversity.edu.in>
2026-07-28 16:24:26 +0530

    Ceiling

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,40 +3,67 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BinarySearch{
-    static int bs(int[] arr, int target, int size){
+public class BinarySearch {
+
+    static int bs(int[] arr, int target, int size) {
         int start = 0;
         int end = size - 1;
         int res = -1;
-        while (start <= end){
-            int mid = start + (end - start)/2;
-            if (arr[mid] == target)
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
                 return mid;
-            if (arr[mid] < target){
+            }
+            if (arr[mid] < target) {
                 start = mid + 1;
-            }else{
+            } else {
                 end = mid - 1;
             }
-        } 
+        }
         return res;
     }
-    public static void main(String[] args) throws IOException{
+
+    static int ceilingOfNumber(int[] arr, int target) {
+        //Ceiling => Number greater that or equal to target
+        int start = 0;
+        int end = arr.length - 1;
+        if (arr[end] < target) {
+            return -1;
+        }
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = 5;
         int[] arr = new int[n];
 
         int target;
-        System.out.println("Enter "+n+" Array Elements (space in between) :");
+        System.out.println("Enter " + n + " Array Elements (space in between) :");
         String[] parts = br.readLine().split(" ");
         System.out.println("Enter Target Element :");
         target = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < parts.length; i++){
+        for (int i = 0; i < parts.length; i++) {
             arr[i] = Integer.parseInt(parts[i]);
         }
 
         int res = bs(arr, target, arr.length);
-        System.out.println(res);
-        
+        int ceil = ceilingOfNumber(arr, target);
+
+        System.out.println("Ceiling : " + ceil);
+        // System.out.println(res);
+
     }
 }
