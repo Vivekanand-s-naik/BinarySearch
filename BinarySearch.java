2de8c6f2c520438a3f3ebca71afcc37f45471d94
Vivekanand-s-naik <vivekanadsnaik.iet@srinivasuniversity.edu.in>
2026-07-31 00:06:23 +0530

    Matrix Search

--- BinarySearch.java
+++ BinarySearch.java
@@ -199,6 +199,23 @@ public class BinarySearch {
         return false;
     }
 
+    public int[] searchMatrixIndex(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return new int[]{row, col};
+            }
+            if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
     public static void main(String[] args) throws IOException {
         // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // int n = 5;
